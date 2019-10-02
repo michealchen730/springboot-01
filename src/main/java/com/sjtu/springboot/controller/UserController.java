6b170f1dad39f0086a5ec8c4c9c4d297481d558c
MichealChen <michealchen730@gmail.com>
2019-10-02 23:07:46 +0800

    some errors

--- src/main/java/com/sjtu/springboot/controller/UserController.java
+++ src/main/java/com/sjtu/springboot/controller/UserController.java
@@ -3,14 +3,15 @@ package com.sjtu.springboot.controller;
 import com.sjtu.springboot.model.User;
 import com.sjtu.springboot.service.serviceImpl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
+
+@RequestMapping("/user")
 public class UserController {
 
     private UserServiceImpl userService;
@@ -20,22 +21,48 @@ public class UserController {
         this.userService=userService;
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public User getUser(@PathVariable("id") Integer id){
         return userService.getUserById(id);
     }
 
-    @GetMapping("/user/all")
+    @GetMapping("/all")
     public ModelAndView selectAll(){
         List<User> list=userService.getAllUser();
         return new ModelAndView("showAllUser","list",list);
     }
 
-    @GetMapping("/user/change")
+    @GetMapping("/change")
     public ModelAndView changeTest(){
         userService.switchAge(4,5);
         List<User> list=userService.getAllUser();
         return new ModelAndView("showAllUser","list",list);
+    }
+    @GetMapping("/login")
+    public ModelAndView toLogin(){
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("login");
+        return mv;
+    }
+
+    @PostMapping("/dologin")
+    public void doLogin(User user){
+        System.out.println(user.getName());
+        System.out.println(user.getPassword());
+
+    }
+
+    @PostMapping("/checkUser")
+    @ResponseBody
+    public String checkUser(String userName){
+        System.out.println(userName);
+        return "chenggong";
+
+    }
+
+    @RequestMapping("/test")
+    public String toTest(){
+        return "test";
     }
 
 
--- src/main/java/com/sjtu/springboot/model/User.java
+++ src/main/java/com/sjtu/springboot/model/User.java
@@ -9,6 +9,8 @@ public class User {
 
     private Integer gender;
 
+    private String password;
+
     public Integer getId() {
         return id;
     }
@@ -39,5 +41,13 @@ public class User {
 
     public void setGender(Integer gender) {
         this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
